9f629441c15e0e11fd33475cd45c633dededc484
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-19 18:23:06 -0300

    wip sanbox styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Head from 'next/head'
+import Link from 'next/link'
+import s from '../styles/MyApp.module.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  if (pageProps.statusCode === 404) return <Component {...pageProps} />
+
+  return (
+    <div className={s.container}>
+      <Head>
+        <title>Fast Contexts Sandbox</title>
+        <meta
+          name="description"
+          content="sandbox to play with some fast contexts solutions"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ul className={s.navigation}>
+        <li>
+          <Link href="/observables">
+            <a>Observables</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/split-context">
+            <a>Split context</a>
+          </Link>
+        </li>
+      </ul>
+      <main className={s.main}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Fast Contexts Sandbox</title>
-        <meta
-          name="description"
-          content="sandbox to play with some fast contexts solutions"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <ul>
-        <li>
-          <Link href="/about">
-            <a>About Us</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog/hello-world">
-            <a>Blog Post</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  )
+  return <main>Click any of the links above to see the examples</main>
 }
 
 export default Home
