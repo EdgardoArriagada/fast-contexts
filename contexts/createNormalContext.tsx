0a4a91b24373f2cd0c508fa659aa5f5b0f080b75
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-20 22:19:56 -0300

    improve normal context

--- contexts/createNormalContext.tsx
+++ contexts/createNormalContext.tsx
@@ -1,5 +1,5 @@
 import React, {
-  useState,
+  useReducer,
   createContext,
   useContext,
   useCallback,
@@ -12,19 +12,38 @@ const WordStateContext = createContext<
   [[string, string], StringDispatch, StringDispatch] | null
 >(null)
 
+type Action = {
+  type: 'updateFirst' | 'updateLast'
+  payload: string
+}
+
+type State = [string, string]
+type Reducer = (state: State, action: Action) => State
+
+const reducer: Reducer = (state, action) => {
+  switch (action.type) {
+    case 'updateFirst':
+      return [action.payload, state[1]]
+    case 'updateLast':
+      return [state[0], action.payload]
+    default:
+      throw new Error()
+  }
+}
+
 type NormalProviderProps = {
   children?: React.ReactNode
 }
 
 const NormalProvider: FC<NormalProviderProps> = ({ children }) => {
-  const [state, setState] = useState<[string, string]>(['', ''])
+  const [state, dispatch] = useReducer<Reducer>(reducer, ['', ''])
 
   const setFirst = useCallback((value: string) => {
-    setState((prev) => [value, prev[1]])
+    dispatch({ type: 'updateFirst', payload: value })
   }, [])
 
   const setLast = useCallback((value: string) => {
-    setState((prev) => [prev[0], value])
+    dispatch({ type: 'updateLast', payload: value })
   }, [])
 
   return (
