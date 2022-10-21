0ba9170bf36fc8bb0a4b460c1f58be42ede65f24
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-21 15:23:07 -0300

    handle current todos

--- containers/current-todos.tsx
+++ containers/current-todos.tsx
@@ -1,18 +1,56 @@
-import React, { FC } from 'react'
+import React, { FC, CSSProperties } from 'react'
 
 import { useTodoStore } from '../stores/todoStore'
 
 interface Props {}
 
+const columStyles: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+}
+
+const elementStyles: CSSProperties = {
+  display: 'flex',
+  gap: '1rem',
+}
+
 const CurrentTodos: FC<Props> = () => {
-  const [currentTodos] = useTodoStore((todoStore) => todoStore.current)
+  const [currentTodos, setCurrentTodos] = useTodoStore(
+    (todoStore) => todoStore.current
+  )
+  const [, setPastTodos] = useTodoStore((todoStore) => todoStore.past)
+
+  const handleCheckboxClick = (id: number) => {
+    console.log('handleCheckboxClick', id)
+    const newCurrentTodos = currentTodos.filter((todo) => todo.id !== id)
+    const newPastTodos = currentTodos.find((todo) => todo.id === id)
+
+    setCurrentTodos({ current: newCurrentTodos })
+
+    setPastTodos((store) => ({
+      past: [...store.past, newPastTodos],
+    }))
+  }
 
   return (
     <>
       <h3>Current Todos</h3>
-      {currentTodos.map((todo) => {
-        return <div key={todo.id}>{todo.text}</div>
-      })}
+      <div style={columStyles}>
+        {currentTodos.map(({ id, text }) => {
+          return (
+            <div key={id} style={elementStyles}>
+              <input
+                onChange={() => handleCheckboxClick(id)}
+                type="checkbox"
+                id={String(id)}
+                name="subscribe"
+                value="newsletter"
+              />
+              <label htmlFor={String(id)}>{text}</label>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
