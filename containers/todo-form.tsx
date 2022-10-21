46adff3c256d01ea911fcd53a8037b0164e47e5a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-21 14:57:44 -0300

    reset form after submit

--- containers/todo-form.tsx
+++ containers/todo-form.tsx
@@ -1,20 +1,24 @@
-import React, { FC } from 'react'
+import React, { useRef, FC } from 'react'
 
 import { useTodoStore } from '../stores/todoStore'
 
 interface Props {}
 
 const TodoForm: FC<Props> = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+
   const [, setNewValue] = useTodoStore((store) => store.current)
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const newTodo = { id: Date.now(), text: e.currentTarget.todo.value }
 
     setNewValue((store) => ({ current: [...store.current, newTodo] }))
+
+    formRef.current?.reset()
   }
 
   return (
-    <form onSubmit={handleOnSubmit}>
+    <form ref={formRef} onSubmit={handleOnSubmit}>
       <label htmlFor="todo">Todo</label>
       <input id="todo" type="text" />
       <button type="submit">Add</button>
